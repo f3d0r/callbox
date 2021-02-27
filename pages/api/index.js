7a85375c3ede0e53e89f0c23243a6ceb0d889bcb
Fedor Paretsky <me@fedor.me>
2021-02-26 22:39:52 -0800

    Fix

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,18 +3,38 @@ const { send } = require('micro')
 
 
 module.exports = (req, res) => {
-  const response = new VoiceResponse();
+  // Use the Twilio Node.js SDK to build an XML response
+  const twiml = new VoiceResponse();
 
-  const gather = response.gather({ finishOnKey: '#' });
-  response.say('You entered ' + gather.toString());
-  
-  // If the user doesn't enter input, loop
-  
-  response.redirect('/voice');
+  /** helper function to set up a <Gather> */
+  function gather() {
+    const gatherNode = twiml.gather({ numDigits: 1 });
+    gatherNode.say('For sales, press 1. For support, press 2.');
+
+    // If the user doesn't enter input, loop
+    twiml.redirect('/voice');
+  }
+
+  // If the user entered digits, process their request
+  if (request.body.Digits) {
+    switch (request.body.Digits) {
+      case '1':
+        twiml.say('You selected sales. Good for you!');
+        break;
+      case '2':
+        twiml.say('You need support. We will help!');
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.").pause();
+        gather();
+        break;
+    }
+  } else {
+    // If no input was sent, use the <Gather> verb to collect user input
+    gather();
+  }
+
   // Render the response as XML in reply to the webhook request
-  response.type('text/xml');
-  response.send(response.toString());
-
   res.setHeader('Content-Type', 'text/xml')
   send(res, 200, response.toString());
 }
