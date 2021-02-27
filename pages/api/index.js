335f12bd3bb9bd02e36c0fe47fc0b6621e401b42
Fedor Paretsky <me@fedor.me>
2021-02-26 22:33:18 -0800

    Fix

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,9 +3,18 @@ const { send } = require('micro')
 
 
 module.exports = (req, res) => {
-  const twiml = new VoiceResponse();
-  twiml.say({ voice: 'alice' }, 'hello world!');
+  const response = new VoiceResponse();
+
+  const gather = response.gather({ finishOnKey: '#' });
+  gather.say('You entered ' + gather.toString());
+  
+  // If the user doesn't enter input, loop
+  
+  response.redirect('/voice');
+  // Render the response as XML in reply to the webhook request
+  response.type('text/xml');
+  response.send(response.toString());
 
   res.setHeader('Content-Type', 'text/xml')
-  send(res, 200, twiml.toString());
+  send(res, 200, response.toString());
 }
